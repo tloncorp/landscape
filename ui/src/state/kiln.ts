29fff1d8421c372ea9fc93c9aecb7d54391119c8
tomholford <tomholford@users.noreply.github.com>
2022-11-03 22:19:36 -0700

    grid: consume Pike interface and scry

--- ui/src/state/kiln.ts
+++ ui/src/state/kiln.ts
@@ -6,30 +6,46 @@ import {
   Vat,
   kilnInstall,
   kilnPause,
-  kilnResume
+  kilnResume,
+  getPikes,
+  Pikes
 } from '@urbit/api';
 import create from 'zustand';
 import produce from 'immer';
 import { useCallback } from 'react';
 import api from './api';
+import { fakeRequest, useMockData } from './util';
+import { mockPikes, mockVats } from './mock-data';
 
 interface KilnState {
   vats: Vats;
+  pikes: Pikes;
   loaded: boolean;
   fetchVats: () => Promise<void>;
   lag: boolean;
   fetchLag: () => Promise<void>;
+  fetchPikes: () => Promise<void>;
   changeOTASource: (ship: string) => Promise<void>;
   toggleOTAs: (desk: string, on: boolean) => Promise<void>;
   set: (s: KilnState) => void;
 }
 const useKilnState = create<KilnState>((set, get) => ({
   vats: {},
+  pikes: useMockData ? mockPikes : {},
   lag: false,
   loaded: false,
   fetchVats: async () => {
     const vats = await api.scry<Vats>(getVats);
     set({ vats, loaded: true });
+  },
+  fetchPikes: async () => {
+    if (useMockData) {
+      await fakeRequest({}, 500);
+      set({ loaded: true });
+      return;
+    }
+    const pikes = await api.scry<Pikes>(getPikes);
+    set({ pikes, loaded: true });
   },
   fetchLag: async () => {
     const lag = await api.scry<boolean>(scryLag);
