714f50ed4f45c5fd562770306561fee6899fb1e5
tomholford <tomholford@users.noreply.github.com>
2022-11-21 16:10:59 -0800

    grid: distinguish app sync from system install
    By using `%kiln-install` instead of `%kiln-sync` for System Updates, this ensures that the `%kids` desk is also updated.
    
    Also, address UX feedback: render the entire source ship's patp to avoid ambiguity. (as opposed to truncating a moon's name).

--- ui/src/preferences/AppPrefs.tsx
+++ ui/src/preferences/AppPrefs.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { useCharge } from '../state/docket';
-import { usePike } from '../../state/kiln';
+import useKilnState, { usePike } from '../state/kiln';
 import { getAppName } from '../state/util';
-import SourceSyncer from '../components/SourceSyncer';
+import SourceSetter from '../components/SourceSetter';
 
 export const AppPrefs = ({ match }: RouteComponentProps<{ desk: string }>) => {
   const { desk } = match.params;
   const charge = useCharge(desk);
   const appName = getAppName(charge);
   const pike = usePike(desk);
-  const syncShip = pike?.sync?.ship;
-    
+  const srcShip = pike?.sync?.ship;
+  const { toggleSync } = useKilnState();
+
   return (
-    <SourceSyncer
+    <SourceSetter
       appName={appName}
       title={`${appName} Settings`}
-      syncDesk={desk}
-      syncShip={syncShip}
+      toggleSrc={toggleSync}
+      srcDesk={desk}
+      srcShip={srcShip}
     />
   );
 };
--- ui/src/preferences/about-system/UpdatePreferences.tsx
+++ ui/src/preferences/about-system/UpdatePreferences.tsx
@@ -1,9 +1,10 @@
 import { Vat } from '@urbit/api';
 import _ from 'lodash';
 import React from 'react';
-import SourceSyncer from '../../components/SourceSyncer';
-import { usePike } from '../../state/kiln';
+import SourceSetter from '../../components/SourceSetter';
+import useKilnState, { usePike } from '../../state/kiln';
 
+// TODO
 interface UpdatePreferencesProps {
   base: Vat | undefined;
 }
@@ -12,9 +13,16 @@ export const UpdatePreferences = ({ base }: UpdatePreferencesProps) => {
   const desk = 'base';
   const appName = 'your Urbit';
   const pike = usePike(desk);
-  const syncShip = pike?.sync?.ship;
-
+  const srcShip = pike?.sync?.ship;
+  const { toggleInstall } = useKilnState();
+  
   return (
-    <SourceSyncer appName={appName} title="System Updates" syncDesk={desk} syncShip={syncShip} />
+    <SourceSetter
+      appName={appName}
+      toggleSrc={toggleInstall}
+      srcDesk={desk}
+      srcShip={srcShip}
+      title="System Updates"
+    />
   );
 };
