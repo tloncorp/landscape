f14013caed4984d2b4f8d441c285f8870383cd97
Philip Monk <phil@pcmonk.me>
2023-01-05 09:08:27 -0700

    preferences: for system, give base hash instead of garden hash

--- ui/src/preferences/about-system/AboutSystem.tsx
+++ ui/src/preferences/about-system/AboutSystem.tsx
@@ -16,12 +16,11 @@ function getHash(pike: Pike): string {
 }
 
 export const AboutSystem = () => {
-  const gardenCharge = useCharge('garden');
-  const gardenPike = usePike(window.desk);
+  const basePike = usePike('base');
   const { systemBlocked, blockedCharges, blockedCount, freezeApps } =
     useSystemUpdate();
   const gardenBlocked = null != blockedCharges.find(charge => charge.desk == 'garden');
-  const hash = gardenPike && getHash(gardenPike);
+  const hash = basePike && getHash(basePike);
   const lag = useLag();
   
   return (
@@ -38,12 +37,8 @@ export const AboutSystem = () => {
         <div className="leading-5 space-y-4">
           <FullTlon16Icon className="h-4" />
           <div>
-            <p>Landscape Operating Environment</p>
-            <p>by Tlon Corporation</p>
-          </div>
-          <div>
             <p>
-              Version {gardenCharge?.version} ({hash})
+              Urbit Kernel Version ({hash})
             </p>
           </div>
           {systemBlocked ? (
