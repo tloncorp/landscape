9e58f6a0cbdd1d7873c5ef9d6134bcc5c4718c91
Hunter Miller <hunter@hmiller.dev>
2023-01-31 12:36:54 -0600

    ops: converting deploy action to bash script

---
+++ .github/helpers/deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# this script deploys a desk to a ship from a github repository
+# assumes gcloud credentials are loaded and gcloud installed.
+
+repo=$1
+desk=$2
+ship=$3
+zone=$4
+folder=$ship/$desk
+
+set -e
+set -o pipefail
+cmds=$(mktemp "${TMPDIR:-/tmp/}janeway.XXXXXXXXX")
+echo '
+source_repo=$(mktemp --dry-run /tmp/repo.janeway.XXXXXXXXX)
+git clone git@github.com:'$repo'.git $source_repo
+urbit_repo=$(mktemp --dry-run /tmp/repo.urbit.XXXXXXXXX)
+git clone git@github.com:urbit/urbit.git $urbit_repo
+cd $source_repo
+git checkout .
+cd /home/urb || return
+curl -s --data '"'"'{"source":{"dojo":"+hood/mount %'$desk'"},"sink":{"app":"hood"}}'"'"' http://localhost:12321
+rsync -avL --delete $source_repo/desk/ '$folder'
+rsync -avL $urbit_repo/pkg/base-dev/ '$folder'
+curl -s --data '"'"'{"source":{"dojo":"+hood/commit %'$desk'"},"sink":{"app":"hood"}}'"'"' http://localhost:12321
+rm -rf $source_repo
+rm -rf $urbit_repo
+' >> "$cmds"
+gcloud compute \
+  --project mainnet \
+  ssh \
+  --ssh-flag="-T" \
+  --zone $4 \
+  urb@$ship < "$cmds" &> /dev/null
+echo "OTA performed for $desk on $ship"
