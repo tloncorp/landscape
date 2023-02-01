8208407f5fc52b8fccc82c4d31fbcd2336b9ea8b
Hunter Miller <hunter@hmiller.dev>
2023-02-01 15:50:55 -0600

    ops: debugging weird git clone

--- .github/helpers/prep-deploy.sh
+++ .github/helpers/prep-deploy.sh
@@ -14,19 +14,21 @@ set -o pipefail
 cmdfile=$(mktemp "${TMPDIR:-/tmp/}janeway.XXXXXXXXX")
 cmds='
 source_repo=$(mktemp --dry-run /tmp/repo.janeway.XXXXXXXXX)
+echo $source_repo;
 git clone git@github.com:'$repo'.git $source_repo
 urbit_repo=$(mktemp --dry-run /tmp/repo.urbit.XXXXXXXXX)
+echo $urbit_repo;
 git clone git@github.com:urbit/urbit.git $urbit_repo
 cd $source_repo
 git checkout '$ref'
 cd /home/urb || return
-curl -s --data '"'"'{"source":{"dojo":"+hood/mount %'$desk'"},"sink":{"app":"hood"}}'"'"' http://localhost:12321
+curl -s --data '\''{"source":{"dojo":"+hood/mount %'$desk'"},"sink":{"app":"hood"}}'\'' http://localhost:12321
 rsync -avL --delete $source_repo/desk/ '$folder'
 rsync -avL $urbit_repo/pkg/base-dev/ '$folder'
-curl -s --data '"'"'{"source":{"dojo":"+hood/commit %'$desk'"},"sink":{"app":"hood"}}'"'"' http://localhost:12321
+curl -s --data '\''{"source":{"dojo":"+hood/commit %'$desk'"},"sink":{"app":"hood"}}'\'' http://localhost:12321
 rm -rf $source_repo
 rm -rf $urbit_repo
 '
 echo "$cmds" >> "$cmdfile"
-
+echo "$cmdfile"
 echo "cmdfile=$(echo $cmdfile)" >> $GITHUB_OUTPUT
