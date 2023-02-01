4a17b0539d5bf87bb5a594882cefd38f9d4e505f
Hunter Miller <hunter@hmiller.dev>
2023-02-01 12:10:58 -0600

    ops: pipe key correctly

--- .github/helpers/prep-deploy.sh
+++ .github/helpers/prep-deploy.sh
@@ -29,6 +29,6 @@ rm -rf $urbit_repo
 echo "$cmds"
 echo "$cmds" >> "$cmdfile"
 
-key=$(base64 -d $SSH_SEC_KEY)
+key=$(echo "$SSH_SEC_KEY" | base64 -d)
 echo "key=$(echo $key)" >> $GITHUB_OUTPUT
 echo "cmdfile=$(echo $cmdfile)" >> $GITHUB_OUTPUT
