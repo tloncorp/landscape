268c8a482d427431f440840730cfe9f8a49668cf
tomholford <tomholford@users.noreply.github.com>
2023-06-08 19:36:49 -0700

    devex: add storybook

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,42 +1,34 @@
 module.exports = {
-  extends: ['@tloncorp/eslint-config', 'plugin:storybook/recommended'],
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parserOptions: {
-        project: ['tsconfig.json'],
-        tsconfigRootDir: __dirname
-      }
+  extends: ['@tloncorp/eslint-config', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+  overrides: [{
+    files: ['**/*.ts', '**/*.tsx'],
+    parserOptions: {
+      project: ['tsconfig.json'],
+      tsconfigRootDir: __dirname
     }
-  ],
+  }],
   rules: {
     semi: [2, 'always'],
     'react/destructuring-assignment': 'off',
     'react/require-default-props': 'off',
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.jsx', '.tsx']
-      }
-    ],
-    'tailwindcss/no-custom-classname': [
-      0,
-      {
-        config: 'tailwind.config.js'
-      }
-    ],
-    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['draft'] }],
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.jsx', '.tsx']
+    }],
+    'tailwindcss/no-custom-classname': [0, {
+      config: 'tailwind.config.js'
+    }],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: ['draft']
+    }],
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      {
-        typedefs: false
-      }
-    ]
+    '@typescript-eslint/no-unused-vars': ['warn', {
+      argsIgnorePattern: '^_',
+      destructuredArrayIgnorePattern: '^_'
+    }],
+    '@typescript-eslint/no-use-before-define': ['error', {
+      typedefs: false
+    }]
   },
   settings: {
     tailwindcss: {
@@ -46,12 +38,7 @@ module.exports = {
       alias: {
         // mapping big-integer and fuzzy to a non-existent to squash error described here:
         // https://github.com/johvin/eslint-import-resolver-alias/issues/18
-        map: [
-          ['@', './src'],
-          ['big-integer', 'hack'],
-          ['fuzzy', 'hack'],
-          ['vitest', 'hack']
-        ],
+        map: [['@', './src'], ['big-integer', 'hack'], ['fuzzy', 'hack'], ['vitest', 'hack']],
         extensions: ['.ts', '.tsx']
       }
     }
